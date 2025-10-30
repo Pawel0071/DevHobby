13a9028e91a85e81de1cca297031702eeebc9016
Paweł Bąkowski <pawel_b@windowslive.com>
2025-10-30 13:44:28 +0100

    added ignore

---
+++ RPG.CLI/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.CommandLine;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using RPG.Application;
+using RPG.Application.Handlers;
+using RPG.CLI.Commands;
+using RPG.Core;
+using RPG.Infrastructure;
+
+var builder = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        config.AddJsonFile("../RPG.Infrastructure/appsettings.infrastructure.json", optional: true, reloadOnChange: true);
+        config.AddJsonFile("../RPG.Core/appsettings.core.json", optional: true, reloadOnChange: true);
+        config.AddJsonFile("../RPG.Application/appsettings.application.json", optional: true, reloadOnChange: true);
+    })
+    .ConfigureServices((context, services) =>
+    {
+        var configuration = context.Configuration;
+        
+        // MediatR
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(CharacterCommandHandler).Assembly);
+        });
+        
+        services.AddInfrastructure(configuration);
+        services.AddCore(configuration);
+        services.AddApplication(configuration);
+        
+    });
+
+using var host = builder.Build();
+var services = host.Services;
+var mediator = services.GetRequiredService<IMediator>();
+
+// CLI root
+var rootCommand = new RootCommand("RPG CLI");
+
+var equipCommand = new EquipCommand(services);
+
+await rootCommand.InvokeAsync(args);
