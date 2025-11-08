e0ec03bd33a6d0ee87c0cf44836950bf12a43308
Paweł Bąkowski <pawel_b@windowslive.com>
2025-11-08 17:12:55 +0100

    next step

--- RPG.Domain/Common/ItemTypeDefinition.cs
+++ RPG.Domain/Common/ItemTypeDefinition.cs
@@ -3,7 +3,7 @@ using RPG.Domain.Enums;
 
 namespace RPG.Domain.Common;
 
-public class ItemTypeDefinition : IDictionaryEntry<ItemTypeDefinition>
+public class TypeDefinition : IDictionaryEntry<TypeDefinition>
 {
     public required string DisplayName { get; init; }
     public IList<string> Tags { get; init; } = new List<string>();
@@ -11,5 +11,5 @@ public class ItemTypeDefinition : IDictionaryEntry<ItemTypeDefinition>
     public IList<Type> RequiredComponents { get; init; } = new List<Type>();
     public IList<Type> OptionalComponents { get; init; } = new List<Type>();
     public string Code { get; init; } = string.Empty;
-    public static IEnumerable<ItemTypeDefinition> Predefined => Enumerable.Empty<ItemTypeDefinition>();
+    public static IEnumerable<TypeDefinition> Predefined => Enumerable.Empty<TypeDefinition>();
 }
